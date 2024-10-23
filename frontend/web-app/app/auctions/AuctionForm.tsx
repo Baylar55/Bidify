5ba21b85674362bb0fdd00e4047a7f6c258fc14d
Baylar55 <bylrsadiqov@gmail.com>
2024-10-23 12:30:54 +0400

    feat: Create reusable text input and update auction form

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -2,16 +2,21 @@
 
 import { log } from "console";
 import { Button, TextInput } from "flowbite-react";
-import React from "react";
+import React, { use, useEffect } from "react";
 import { FieldValues, useForm } from "react-hook-form";
+import Input from "../components/Input";
 
 export default function AuctionForm() {
   const {
-    register,
+    control,
     handleSubmit,
     setFocus,
     formState: { isSubmitting, isValid, isDirty, errors },
-  } = useForm();
+  } = useForm({
+    mode: "onTouched",
+  });
+
+  useEffect(() => { setFocus('make') }, [setFocus]);
 
   function onSubmit(data: FieldValues) {
     log(data);
@@ -19,26 +24,36 @@ export default function AuctionForm() {
 
   return (
     <form className="flex flex-col mt-3" onSubmit={handleSubmit(onSubmit)}>
-      <div className="mb-3 block">
-        <TextInput
-          {...register("make", { required: "Make is required" })}
-          placeholder="Make"
-          color={errors?.make && "failure"}
-          helperText={errors?.make?.message as string}
-        />
+
+      <Input label="Make" name="make" control={control} rules={{required: "Make is required"}} />
+      
+      <Input label="Model" name="model" control={control} rules={{required: "Model is required"}} />
+
+      <Input label="Color" name="color" control={control} rules={{required: "Color is required"}} />
+      
+      <div className="grid grid-cols-2 gap-3">
+        
+        <Input label="Year" name="year" control={control} type="number" rules={{required: "Year is required"}} />
+
+        <Input label="Mileage" name="mileage" control={control} type="number" rules={{required: "Mileage is required"}} />
+      
       </div>
-      <div className="mb-3 block">
-        <TextInput
-          {...register("model", { required: "Model is required" })}
-          placeholder="Model"
-          color={errors?.model && "failure"}
-          helperText={errors?.model?.message as string}
-        />
+
+      <Input label="Image URl" name="imageUrl" control={control} rules={{required: "Image URL is required"}} />
+
+      <div className="grid grid-cols-2 gap-3">
+        
+        <Input label="Reserve Price (enter 0 if no reserve)" name="reservePrice" control={control} type="number" rules={{required: "Reserve price is required"}} />
+
+        <Input label="Auction end date/time" name="autionEnd" control={control} type="date" rules={{required: "Auction's end date is required"}} />
+      
       </div>
+
       <div className="flex justify-between">
         <Button outline color="gray">Cancel</Button>
         <Button isProcessing={isSubmitting} disabled={!isValid} type="submit" outline color="success">Submit</Button>
       </div>
+      
     </form>
   );
 }
